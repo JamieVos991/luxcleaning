5965de75d5f8028ee7a1be0c98c490280637d477
Jamie Vos <Jamievos100@gmail.com>
2026-02-01 16:47:19 +0100

    Go to top knop gemaakt met js, html en css (#6)

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,19 @@
 const slider = document.querySelector('.slider');
 const overlay = document.querySelector('.image-overlay');
 const button = document.querySelector('.slider-button');
+const naarBovenKnop = document.querySelector('#naarBovenKnop')
 
+naarBovenKnop.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+})
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 800) {
+        naarBovenKnop.removeAttribute('hidden');
+    } else {
+        naarBovenKnop.setAttribute('hidden', '');
+    }
+  });
   
 
 slider.addEventListener('input', (e) => {
